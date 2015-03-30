34b2c062f090174eb8a8fba60b35de9cc4c04268
Dan O'Huiginn <daniel@ohuiginn.net>
2015-03-30 11:47:05 +0200

    get .gitignore in line; settings template

--- oilmodels/settings.templates.py
+++ oilmodels/settings.templates.py
@@ -77,8 +77,11 @@ WSGI_APPLICATION = 'oilmodels.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'openoil',
+        'USER': 'openoil',
+        'PASSWORD': 'XXXXXXXXXXXXXXX',
+        #'HOST': '144.76.203.23',
     }
 }
 
