9e260c315878f32a6b64a0513fdde72ab3c973d4
Dan O'Huiginn <daniel@ohuiginn.net>
2015-04-29 13:21:08 +0200

    Paging for API
    fixes #7

--- ooapi/views.py
+++ ooapi/views.py
@@ -8,7 +8,26 @@ def concessions(request):
     query = Concession.objects.all()
     if 'country' in request.GET:
         query = query.filter(country=request.GET['country'])
-    output = concs_as_dicts(query)
+
+    # offset/limit
+
+    page = int(request.GET.get('page', 1))
+    per_page = int(request.GET.get('per_page', 100))
+    index_start = (page-1) * per_page
+    index_end = index_start + per_page
+        
+    query = query[index_start:index_end]
+
+    results = concs_as_dicts(query)
+
+    output = {
+        'api_version': 0.1,
+        'result_count': query.count(),
+        'page': page,
+        'per_page': per_page,
+        'results': results
+    }
+    
     return JsonResponse(output, safe=False)
 
 def concs_as_dicts(matches):
