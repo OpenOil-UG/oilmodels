c59edc2cc68aa61f16a1c25a6f74e6d1bb405adb
Dan O'Huiginn <daniel@ohuiginn.net>
2015-04-30 17:46:30 +0200

    API: allow urls with/without version number

--- ooapi/urls.py
+++ ooapi/urls.py
@@ -2,8 +2,13 @@
 from django.conf.urls import include, url
 from ooapi import views
 
-urlpatterns = [
+v01_urlpatterns = [
     # Examples:
     url(r'^documentation/?', views.documentation, name="documentation"),
     url(r'^concessions/?', views.concessions, name="concessions"),
 ]
+
+urlpatterns = [
+    url(r'^v0.1/', include(v01_urlpatterns)),
+    url(r'^/?', include(v01_urlpatterns)),
+    ]
